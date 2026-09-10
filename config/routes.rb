12007826a1cd08c28d741e8f6adf4a478af5ae50
fuku-cpu <hirogoose227@gmail.com>
2026-09-10 17:39:14 +0900

    家事詳細画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :chores, only: %i[index new create]
+  resources :chores, only: %i[index new create show]
 end
